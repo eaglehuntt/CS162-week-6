32e7366fedf594a03187d5a0fe255d5e09bf2f24
eaglehuntt <eagle@eaglehunt.net>
2023-11-07 11:32:08 -0800

    Decided to make more sorting algorithms as a class

---
+++ sort_algorithms.py
@@ -0,0 +1,46 @@
+class Sort:
+
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def merge_sort(array):
+
+        def merge(a,b):
+            c = []
+            while len(a) >= 1 and len(b) >= 1:
+                if a[0] < b[0]:
+                    c.append(a[0])
+                    a.pop(0)
+                else:
+                    c.append(b[0])
+                    b.pop(0)
+            
+            while len(a) >= 1:
+                c.append(a[0])
+                a.pop(0)
+            
+            while len(b) >= 1:
+                c.append(b[0])
+                b.pop(0)
+
+            return c
+        
+
+        length = len(array)
+
+        if length == 1:
+            return array
+
+        a = array[:length // 2]
+        b = array[length // 2:]
+
+        a = Sort.merge_sort(a)
+        b = Sort.merge_sort(b)
+
+
+        return merge(a, b)
+
+
+
+print(Sort.merge_sort([1,8,5,4,2,4,2,4,5,6,7,8]))
